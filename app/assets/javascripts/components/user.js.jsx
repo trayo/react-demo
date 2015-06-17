ee0804c89bfbf1361c7009844479db545b00f85f
Travis Yoder <trayoda@gmail.com>
2015-06-17 15:16:28 -0600

    added code that allows you to edit a Users name in place

--- app/assets/javascripts/components/user.js.jsx
+++ app/assets/javascripts/components/user.js.jsx
@@ -1,17 +1,56 @@
 var User = React.createClass({
+  getInitialState: function() {
+    return {
+      user: this.props.user,
+      name: this.props.user.name,
+      isEditing: true,
+    };
+  },
+
+  handleChange: function(e) {
+    this.setState({name: e.target.value});
+  },
+
+  flipEditing: function() {
+    this.setState({ isEditing: !this.state.isEditing });
+  },
+
+  handleSubmit: function(name) {
+    var newName = this.refs.name.getDOMNode().value.trim();
+    var userID = this.state.user.id;
+
+    $.ajax({
+      url: "/users/" + userID + ".json",
+      dataType: 'json',
+      type: 'PUT',
+      data: {"id": userID, "name": newName},
+    });
+
+    this.flipEditing();
+  },
+
   render: function() {
     return (
       <div>
-        <h2>
-          {this.props.user.name}<br/>
-          by: {this.props.user.id}
-        </h2>
+        <h2 hidden={!this.state.isEditing}> {this.state.name} </h2>
+        <form onSubmit={this.handleSubmit} hidden={this.state.isEditing}>
+          <input type="text" onChange={this.handleChange} value={this.state.name} ref="name" />
+          <input className="button" type="submit" value="Save" />
+        </form>
+        by: {this.state.user.id} <br/>
+        <button onClick={this.flipEditing}>Edit User</button> <br/>
       </div>
     );
   }
 });
 
+// <UserEdit onClick={this.changeName} />
+
 var UserList = React.createClass({
+  getInitialState: function() {
+    return {users: this.props.users};
+  },
+
   render: function() {
     var createUser = function(user){
       return <User user={user} />;
@@ -23,7 +62,7 @@ var UserList = React.createClass({
 
     return (
       <LiveSearch
-        library={this.props.users}
+        library={this.state.users}
         filterTerm={filterTerm}
         display={createUser}
       />
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,4 +4,9 @@ class UsersController < ApplicationController
   def index
     respond_with @users = User.all
   end
+
+  def update
+    @user = User.find(params[:id])
+    respond_with @user.update_attributes(name: params[:name])
+  end
 end
